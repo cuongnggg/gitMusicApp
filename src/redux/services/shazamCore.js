55bf34428048d48790e54564d58a6327cc3d44a0
cuong <cuongnguyenkid@gmail.com>
2023-04-09 15:59:51 +0700

    [Apr923][Cuong] fix JsonError

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,6 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '867e50fec1msh8d6556183a9384ep1346f7jsn067cac628c42',
+      'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+    
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
